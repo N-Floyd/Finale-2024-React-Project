8a2d4b6b52ad3b5287ea1d73db8c1dbec1dd9df4
N-floyd <beriashvili.nino91@gmail.com>
2024-10-01 10:45:34 +0400

    Main

---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='discount flex justify-center gap-2 max-w-[1440px] w-[1440px] h-[40px] bg-black'>
+     <div className='firstText text-[14px] text-white font-[400] mt-[10px]'>Get 25% OFF on your first order.</div>
+     <div className='secondText text-[14px] text-white font-[500] mt-[10px]'>Order Now</div>
+    </div>
+  )
+}
+
+export default Main
