af94a5f1edc5729e5f6e0293b550d6740f4b2963
N-floyd <beriashvili.nino91@gmail.com>
2024-10-09 17:45:08 +0400

    After Payment Failed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Product from './pages/Product';
 import Listing from './pages/Listing';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
-import AfterPaymentSucces from './pages/AfterPaymentSucces';
+import AfterPaymentS from './pages/AfterPaymentS';
+import AfterPaymentF from './pages/AfterPaymentF';
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
         <Route path="/Listing" element={<Listing />} />
         <Route path="/Cart" element={<Cart />} />
         <Route path="/Checkout" element={<Checkout />} />
-        <Route path="/AfterPaymentSucces" element={<AfterPaymentSucces />} />
+        <Route path="/AfterPaymentS" element={<AfterPaymentS />} />
+        <Route path="AfterPaymentF" element={<AfterPaymentF />} />
       </Routes>
     </div>
   );
