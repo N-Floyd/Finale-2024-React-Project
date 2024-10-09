b9afacb2edf1646c3ce7cd34c0ca16b8c50e463a
N-floyd <beriashvili.nino91@gmail.com>
2024-10-09 14:31:50 +0400

    Checkout all done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Product from './pages/Product';
 import Listing from './pages/Listing';
 import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/product" element={<Product />} />
         <Route path="/Listing" element={<Listing />} />
         <Route path="/Cart" element={<Cart />} />
+        <Route path="/Checkout" element={<Checkout />} />
       </Routes>
     </div>
   );
