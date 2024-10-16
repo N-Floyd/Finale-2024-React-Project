abce98f1cd4ed5c6ad2043623f6363ca45709647
N-floyd <beriashvili.nino91@gmail.com>
2024-10-16 22:17:45 +0400

    Improvement of Components

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ import Adress from './pages/Adress';
 import Details from './pages/Details';
 import Password from './pages/Password';
 import StartShop from './pages/StartShop';
-import AdminLogin from './pages/AdminPages/AdminLogin';
-import Dashboard from './pages/AdminPages/Dashboard';
+import AdminLogin from './AdminPage/AdminLogin';
+import Dashboard from './AdminPage/Dashboard';
 
 
 function App() {
@@ -27,23 +27,23 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/product" element={<Product />} />
-        <Route path="/Listing" element={<Listing />} />
-        <Route path="/Cart" element={<Cart />} />
-        <Route path="/Checkout" element={<Checkout />} />
-        <Route path="/AfterPaymentS" element={<AfterPaymentS />} />
-        <Route path="AfterPaymentF" element={<AfterPaymentF />} />
-        <Route path="Login" element={<Login />} />
-        <Route path="SignUp" element={<SignUp />} />
-        <Route path="ForgotP" element={<ForgotP />} />
-        <Route path="ResetP" element={<ResetP />} />
-        <Route path='Orders' element={<Orders />} />
-        <Route path='Wishlist' element={<Wishlist />} />
-        <Route path='Adress' element={<Adress />} />
-        <Route path='Details' element={<Details />} />
-        <Route path='Password' element={<Password />} />
-        <Route path='StartShop' element={<StartShop />} />
-        <Route path='AdminLogin' element={<AdminLogin />} />
-        <Route path='Dashboard' element={<Dashboard />} />
+        <Route path="/listing" element={<Listing />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/afterPaymentS" element={<AfterPaymentS />} />
+        <Route path="/afterPaymentF" element={<AfterPaymentF />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/forgotP" element={<ForgotP />} />
+        <Route path="/resetP" element={<ResetP />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/wishlist' element={<Wishlist />} />
+        <Route path='/adress' element={<Adress />} />
+        <Route path='/details' element={<Details />} />
+        <Route path='/password' element={<Password />} />
+        <Route path='/startShop' element={<StartShop />} />
+        <Route path='/adminLogin' element={<AdminLogin />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </div>
   );
