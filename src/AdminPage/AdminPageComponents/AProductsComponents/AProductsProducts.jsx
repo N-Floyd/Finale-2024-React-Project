632e77ebe9246abd0770c5358fe205c0527d8884
N-floyd <beriashvili.nino91@gmail.com>
2024-10-18 17:04:25 +0400

    Error

---
+++ src/AdminPage/AdminPageComponents/AProductsComponents/AProductsProducts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AProductsProducts = () => {
+  return (
+    <div>AProductsProducts</div>
+  )
+};
+
+export default AProductsProducts;
---
+++ src/AdminPage/AdminPageComponents/AProductsComponents/ComponentProperties.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ComponentProperties = () => {
+  return (
+    <div>ComponentProperties</div>
+  )
+};
+
+export default ComponentProperties;
